acfe4f2b2f373b8e4237240272dae724b85b3aee
Allen Jones <allen.jones@gmail.com>
2022-01-10 16:15:21 +0000

    Create seed route and seed data to test. Add code to index.ejs to display data from database.

--- controllers/courses.js
+++ controllers/courses.js
@@ -3,11 +3,36 @@ const coursesRouter = express.Router();
 const Course = require('../models/course');
 const { append } = require('express/lib/response');
 
+// Seed Route
+const courseSeed = require('../models/courseSeed.js');
+
+coursesRouter.get('/seed', (req, res) => {
+    Course.deleteMany({}, (error, allCourses) => { });
+    Course.create(courseSeed, (error, data) => {
+        res.redirect('/courses');
+    });
+});
 
 // Index Route
 coursesRouter.get('/', (req, res) => {
-    res.render('index.ejs', {
-        courses: allCourses,
+    Course.find({}, (error, allCourses) => {
+        res.render('index.ejs', {
+            courses: allCourses,
+        });
+    });
+});
+
+// New Route
+coursesRouter.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+// Edit Route
+coursesRouter.get('/:id/edit', (req, res) => {
+    Course.findById(req.params.id, (error, foundCourse) => {
+        res.render('edit.ejs', {
+            couse: foundCourse,
+        });
     });
 });
 
---
+++ models/courseSeed.js
@@ -0,0 +1,12 @@
+module.exports = [{
+    name: 'Bryant Lake Disc Golf Course',
+    location: 'Eden Prairie, Minnesota',
+    zipcode: '55344',
+    holes: '18',
+    waterHoles: true,
+    teeType: 'Poured Concrete',
+    cost: '6.45',
+    beginnerFriendly: false,
+    description: 'Challenging championship design overlooking scenic lake. Rolling hills with elevation changes. Tee boxes and pin locations manicured immaculately. Alt. tees and pin placements. Many park amenities including concessions/pro shop.' 
+},
+];

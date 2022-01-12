71cec5a02341ab8a7630bbb6cad337d687f94640
Allen Jones <allen.jones@gmail.com>
2022-01-12 22:23:31 +0000

    add redirect for root to courses index page

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo is connected'));
 db.on('disconnected', () => console.log('mongo is disconnected'));
 
+// Routes
+app.get('/', (req, res) => {
+    res.redirect('/courses');
+}); 
+
 // Listener
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));

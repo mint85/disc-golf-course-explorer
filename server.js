42787471da5e2ed3bb290de853590cf8c958ab80
Allen Jones <allen.jones@gmail.com>
2022-01-12 20:34:34 +0000

    Add bootstrap css framework. Add hero image on index page. Add basic css to all pages.

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ require('dotenv').config();
 
 // Middleware
 app.use(morgan('dev'));
-app.use('/public/', express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+app.use('/public/', express.static('public'));
 app.use(methodOverride('_method'));
 
 app.use('/courses', coursesController);
